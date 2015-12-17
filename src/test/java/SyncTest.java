2335ebb91b35216b9f32bb92577f0ca8b00e2ed7
Vladislav Fedotov <vladislav.fedotov@genesys.com>
2015-12-17 14:24:09 +0300

    First sample test was created, also added support of slf4j,  DDL and DML scripts added.

--- src/test/java/SyncTest.java
+++ src/test/java/SyncTest.java
@@ -1,5 +1,41 @@
-/**
- * Created by vfedotov on 12/16/2015.
- */
+import com.github.davidmoten.rx.jdbc.Database;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SyncTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SyncTest.class);
+    private Connection connection;
+    private Database db;
+
+    @Before
+    public void setUp() throws Exception {
+        Class.forName("org.h2.Driver");
+        String url = "jdbc:h2:mem:test";
+        String urlWithInitScripts = url+";DB_CLOSE_DELAY=-1;INIT=runscript from '" +
+                "./src/test/resources/db/sql/create-db" +
+                ".sql'\\;runscript from './src/test/resources/db/sql/insert-data.sql'";
+        connection = DriverManager.getConnection(urlWithInitScripts);
+        db = Database.from(url);
+    }
+
+    @Test
+    public void testSimpleSyncSelect() throws Exception {
+        String selectedEmail = db.select("SELECT email FROM users")
+                .getAs(String.class)
+                .toBlocking()
+                .first();
+        LOGGER.info(selectedEmail);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        connection.close();
+    }
 }
